10c5b86306e880696112048389e49e697fde5254
BlueCodeThree <carliemhamilton@gmail.com>
2019-08-31 11:11:26 +1000

    added gigasecond

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,6 +4,21 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (year % 400 == 0)
+        {
+            return true;
+        }
+        else if (year % 100 == 0)
+        {
+            return false;
+        }
+        else if (year % 4 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
