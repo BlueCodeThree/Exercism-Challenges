5ff057f289ee52a859794472eae6c02567434e84
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-01 17:37:39 +1000

    finished resistor-color.cs

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private static string[] ResistorColours = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    // recommended by D to not use lambda functions here
+    public static int ColorCode(string color) => Array.IndexOf(ResistorColours, color);
+
+    public static string[] Colors() => ResistorColours;
+}
