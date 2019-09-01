3a96085b8030ce9dd8ea7a74f564410d1d04d0ab
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-01 17:43:42 +1000

    undid lambdas

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -4,8 +4,13 @@ public static class ResistorColor
 {
     private static string[] ResistorColours = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
 
-    // recommended by D to not use lambda functions here
-    public static int ColorCode(string color) => Array.IndexOf(ResistorColours, color);
+    public static int ColorCode(string color)
+    {
+        return Array.IndexOf(ResistorColours, color);
+    }
 
-    public static string[] Colors() => ResistorColours;
+    public static string[] Colors()
+    {
+        return ResistorColours;
+    }
 }
